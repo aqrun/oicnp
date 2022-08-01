ddafefe0dff35f1ed7666b54365c62be69b9c0c3
Aqrun <aqrun@sina.com>
2022-08-01 21:54:09 +0800

    locale modify

--- oicnp_yew/src/lib.rs
+++ oicnp_yew/src/lib.rs
@@ -8,10 +8,10 @@ pub use app::*;
 
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-pub fn run_app() -> Result<(), JsValue> {
-    wasm_logger::init(wasm_logger::Config::default());
-    yew::start_app::<App>();
-    Ok(())
-}
+// #[wasm_bindgen]
+// pub fn run_app() -> Result<(), JsValue> {
+//     wasm_logger::init(wasm_logger::Config::default());
+//     yew::start_app::<App>();
+//     Ok(())
+// }
 
