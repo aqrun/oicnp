816889971edf3f83c4fe07e362310fe3364f02e7
Aqrun <aqrun@oicnp.com>
2022-08-29 09:05:30 +0800

    feat: add utils

---
+++ migration/src/utils/comment.rs
@@ -0,0 +1,26 @@
+use sea_orm_migration::prelude::*;
+
+pub async fn comment_table(
+    db: &DatabaseConnection,
+    table_name: &str,
+    comment: &str,
+) -> Result<(), DbErr> {
+    db.execute(Statement::from_string(
+        db.get_database_backend(),
+        format!("COMMENT ON TABLE \"{}\" IS \"{}\";", table_name, comment),
+    ))
+    .await?
+}
+
+pub async fn comment_column(
+    db: &DatabaseConnection,
+    table_name: &str,
+    column_name: &str,
+    comment: &str,
+) -> Result<(), DbErr> {
+    db.execute(Statement::from_string(
+        db.get_database_backend(),
+        format!("COMMENT ON COLUMN \"{}\".{} IS \"{}\";", table_name, column_name, comment),
+    ))
+    .await?
+}
