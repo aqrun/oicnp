d6d5d3fe0aea773cd18ac693e20d34adbf504d54
Aqrun <aqrun@oicnp.com>
2022-09-02 09:40:17 +0800

    feat: add operation_logs

--- migration/src/m20220825_212030_create_operation_logs_table.rs
+++ migration/src/m20220825_212030_create_operation_logs_table.rs
@@ -12,12 +12,46 @@ impl MigrationName for Migration {
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!()
+        let table = Table::create()
+            .table(SysMenus::Table)
+            .if_not_exists()
+            .col(
+                ColumnDef::new(SysOperationLogs::Id)
+                    .string_len(32)
+                    .not_null()
+                    .primary_key()
+                    .unique_key(),
+            )
+            .col(ColumnDef::new(SysOperationLogs::TimeId).biginteger().not_null().default(0))
+            .col(ColumnDef::new(SysOperationLogs::Title).string_len(50).default(""))
+            .col(ColumnDef::new(SysOperationLogs::BussinessType).string_len(100).default(""))
+            .col(ColumnDef::new(SysOperationLogs::Method).string_len(100).default(""))
+            .col(ColumnDef::new(SysOperationLogs::RequestMethod).string_len(100).default(""))
+            .col(ColumnDef::new(SysOperationLogs::OperatorType).string_len(100).default(""))
+            .col(ColumnDef::new(SysOperationLogs::Name).string_len(50).default(""))
+            .col(ColumnDef::new(SysOperationLogs::DepartmentName).string_len(50).default(""))
+            .col(ColumnDef::new(SysOperationLogs::Url).string_len(255).default(""))
+            .col(ColumnDef::new(SysOperationLogs::Ip).string_len(50).default(""))
+            .col(ColumnDef::new(SysOperationLogs::Location).string_len(255).default(""))
+            .col(ColumnDef::new(SysOperationLogs::Param).text().default(""))
+            .col(ColumnDef::new(SysOperationLogs::PathParam).text().default(""))
+            .col(ColumnDef::new(SysOperationLogs::JsonResult).text().default(""))
+            .col(ColumnDef::new(SysOperationLogs::Status).char_len(1).default("1"))
+            .col(ColumnDef::new(SysOperationLogs::ErrorMessage).text("").default(""))
+            .col(ColumnDef::new(SysOperationLogs::Duration).biginteger().default(1))
+            .col(
+                ColumnDef::new(SysOperationLogs::CreatedAt)
+                    .date_time()
+                    .not_null()
+                    .extra("DEFAULT CURRENT_TIMESTAMP".to_string()),
+            )
+            .to_owned();
+
+        manager.create_table(table).await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!()
+        
+        
     }
 }
