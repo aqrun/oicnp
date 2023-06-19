3a9d03f312ab87215d2b3697c0ad932a37b19d43
Aqrun <aqrun@sina.com>
2023-06-20 00:33:06 +0800

    feat: add slugify funcs

--- oicnp-cmd/src/cmd/my_test.rs
+++ oicnp-cmd/src/cmd/my_test.rs
@@ -14,7 +14,9 @@ use oicnp_core::{
         },
         cms_nodes, cms_node_body, cms_taxonomies,
     },
-    utils::get_config_file_path,
+    utils::{get_config_file_path,
+            slugify_paths_without_date, capture_file_name,
+    },
     models::{
         Node, NodeBody,
     }
@@ -31,7 +33,27 @@ pub async fn run() {
     // println!("res------ {:?}", res);
     // find_node_with_body(db).await;
     // find_nodes_by_taxonomy(db).await;
-    get_config_path();
+    // get_config_path();
+    get_slug_url();
+}
+
+/// slug 函数测试
+fn get_slug_url() {
+    let s = "2014-02-24-php代码展示";
+    let slug = slugify_paths_without_date(s);
+
+    let s1 = "2014-02-24-php代码展示.md";
+    let res1 = slugify_paths_without_date(s1);
+
+    let s2 = "content\\diary\\2017-10-07-程序员提高方式.md";
+    let res2 = slugify_paths_without_date(s2);
+    let s3 = "content/diary/2017-10-07-程序员提高方式.md";
+    let res3 = slugify_paths_without_date(s3);
+
+    println!("{}: {}", s, slug);
+    println!("{}: {}", s1, res1);
+    println!("{}: {}", s2, res2);
+    println!("{}: {}", s3, res3);
 }
 
 pub fn get_config_path() {
--- oicnp-core/src/utils/mod.rs
+++ oicnp-core/src/utils/mod.rs
@@ -1,7 +1,9 @@
 mod app_config;
 mod uuid;
 mod config;
+mod slug_utils;
 
 pub use app_config::*;
 pub use uuid::*;
 pub use config::*;
+pub use slug_utils::*;
