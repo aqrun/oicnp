7f5c04716d3363e6bf3696230b5e9e8ef72cb05e
Aqrun <aqrun@sina.com>
2023-06-25 16:07:39 +0800

    feat: add tools

--- oicnp-frontend/src/constants/common.ts
+++ oicnp-frontend/src/constants/common.ts
@@ -1,3 +1,7 @@
 
 export const IS_CLIENT = typeof window !== 'undefined';
 export const WIN = (IS_CLIENT ? window : undefined) as Window;
+export const IS_DEV = process.env.NODE_EVN === 'development'
+  || (WIN?.location && WIN?.location?.href.indexOf('localhost') > 0);
+
+export const API_PORT = 8000;
