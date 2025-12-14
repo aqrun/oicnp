23bd6d3c3c4c3984e982c185f31c58133cc8dd57
Aqrun <aqrun@sina.com>
2025-12-14 21:05:35 +0800

    feat: add pm2 config

---
+++ scrips/frontend.config.js
@@ -0,0 +1,29 @@
+// pm2 start frontend.config.js
+
+module.exports = {
+  apps: [
+    {
+      name: 'lxage-web',
+      cwd: './apps/web',
+      script: 'node_modules/.bin/next',
+      args: 'start -p 9000',
+      instances: 'max',
+      exec_mode: 'cluster',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 9000
+      }
+    },
+    {
+      name: 'lxage-admin',
+      cwd: './apps/admin',
+      script: 'node_modules/.bin/next',
+      args: 'start -p 9001',
+      instances: 1,
+      env: {
+        NODE_ENV: 'production',
+        PORT: 9001
+      }
+    }
+  ]
+};
