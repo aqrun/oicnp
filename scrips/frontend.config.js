e6ddc94385be87775015a6d80a295e191a662b80
Aqrun <aqrun@sina.com>
2025-12-14 22:28:34 +0800

    feat: config modify

--- scrips/frontend.config.js
+++ scrips/frontend.config.js
@@ -4,26 +4,45 @@ module.exports = {
   apps: [
     {
       name: 'lxage-web',
-      cwd: './apps/web',
+      cwd: '/workspace/github.com/aqrun/oicnp/apps/web',
       script: 'node_modules/.bin/next',
       args: 'start -p 9000',
-      instances: 'max',
-      exec_mode: 'cluster',
+      instances: 1,
+      exec_mode: 'fork',
+      watch: false,
+      max_memory_restart: '500M',
       env: {
         NODE_ENV: 'production',
-        PORT: 9000
-      }
+        PORT: 9000,
+        HOSTNAME: '0.0.0.0',
+        NODE_OPTIONS: '--max-old-space-size=500'
+      },
+      error_file: '/var/log/pm2/lxage-web-error.log',
+      out_file: '/var/log/pm2/lxage-web-out.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+      merge_logs: true,
+      log_type: 'json'
     },
     {
       name: 'lxage-admin',
-      cwd: './apps/backend',
+      cwd: '/workspace/github.com/aqrun/oicnp/apps/backend',
       script: 'node_modules/.bin/next',
       args: 'start -p 9001',
       instances: 1,
+      exec_mode: 'fork',
+      watch: false,
+      max_memory_restart: '500M',
       env: {
         NODE_ENV: 'production',
-        PORT: 9001
-      }
+        PORT: 9001,
+        HOSTNAME: '0.0.0.0',
+        NODE_OPTIONS: '--max-old-space-size=1024',
+      },
+      error_file: '/var/log/pm2/lxage-admin-error.log',
+      out_file: '/var/log/pm2/lxage-admin-out.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+      merge_logs: true,
+      log_type: 'json'
     }
   ]
 };
